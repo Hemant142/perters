2a75acf1122eed475675bfc4eae9d9de3bddc11c
Hemant142 <yhemant203@gmail.com>
2024-01-05 19:14:41 +0530

    HEllo

--- peters/src/App.js
+++ peters/src/App.js
@@ -6,12 +6,14 @@ import MainRoutes from './Components/MainRoutes';
 function App() {
   return (
     <div className="App" style={{display:"flex",margin:"0px",padding:"0px",width:"100%"}}>
- <div style={{width:"249px",backgroundColor:"#181818"}}>
+ {/* <div style={{width:"249px",backgroundColor:"#181818"}}>
  <Sidebar />
  </div>
   <div style={{width:"calc(100% - 249px)",backgroundColor:"#FAFAFA"}}>
   <MainRoutes />
-  </div>
+  </div> */}
+
+  <h1>Yaa Hoo</h1>
  </div>
   );
 }
